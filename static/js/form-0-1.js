01afd5b5a00fe8b4d4323dd7bf56ea41dc5c3598
holistudio <daniel.bin.lu@gmail.com>
2021-07-23 23:52:54 -0400

    alternate csv file reading added as comments

--- static/js/form-0-1.js
+++ static/js/form-0-1.js
@@ -13,7 +13,38 @@ let printOnce = true;
 let p = 0;
 
 
+//TODO: Consider reading csv files using the code below instead
+// TODO: see if this prevents timing issues, as p5 may have cause threejs loops to run faster?
 
+// var obj_csv = {
+//     size:0,
+//     dataFile:[]
+// };
+ 
+// function readImage(input) {
+//     console.log(input)
+//  if (input.files && input.files[0]) {
+//  let reader = new FileReader();
+//         reader.readAsBinaryString(input.files[0]);
+//  reader.onload = function (e) {
+//  console.log(e);
+//  obj_csv.size = e.total;
+//  obj_csv.dataFile = e.target.result
+//             console.log(obj_csv.dataFile)
+//             parseData(obj_csv.dataFile)
+            
+//  }
+//  }
+// }
+ 
+// function parseData(data){
+//     let csvData = [];
+//     let lbreak = data.split("\n");
+//     lbreak.forEach(res => {
+//         csvData.push(res.split(","));
+//     });
+//     console.table(csvData);
+// }
 function loadPoints(table){
     let formPoints =  [];
     // let curve = {id:0,points:[]};
